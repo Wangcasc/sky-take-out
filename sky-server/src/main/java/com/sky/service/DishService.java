4c517cd42a1df387494b4d44dc30b7b3441321e5
Wang Xilai <1257811052@qq.com>
2024-12-07 22:52:12 +0800

    新增添加菜品业务实现

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -5,7 +5,7 @@ import com.sky.dto.DishDTO;
 public interface DishService {
 
     /**
-     * 新增菜品
+     * 新增菜品和口味 需要操作两张表 需要保证数据的一致性 需要使用事务
      * @param dishDTO 菜品信息
      */
     public void addDishWithFlavor(DishDTO dishDTO);
