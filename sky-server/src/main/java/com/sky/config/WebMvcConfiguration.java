3378d9c49f05f3508d71e6a12e317fa194592939
Wang Xilai <1257811052@qq.com>
2024-12-09 17:31:28 +0800

    更新Swagger配置，区分管理人员和用户

--- sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebMvcConfiguration.java
@@ -33,7 +33,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     /**
      * 注册自定义拦截器
      *
-     * @param registry
+     * @param registry 拦截器注册器
      */
     protected void addInterceptors(InterceptorRegistry registry) {
         log.info("开始注册自定义拦截器...");
@@ -44,7 +44,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
 
     /**
      * 通过knife4j生成接口文档 管理员接口
-     * @return
+     * @return Docket
      */
     @Bean
     public Docket docket1() {
@@ -87,7 +87,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     /**
      * 设置静态资源映射 这里重写的方法 不可自定义方法名
      * 静态资源映射 用于访问swagger-ui.html 绑定访问路径：http://localhost:8080/doc.html
-     * @param registry
+     * @param registry 静态资源注册器
      */
     protected void addResourceHandlers(ResourceHandlerRegistry registry) {
         log.info("开始设置静态资源映射...");
@@ -98,7 +98,7 @@ public class WebMvcConfiguration extends WebMvcConfigurationSupport {
     /**
      * 扩展SpringMVC的消息转换器
      * 统一对后端返回的数据进行处理
-     * @param converters
+     * @param converters 消息转换器列表
      */
     @Override
     protected void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
