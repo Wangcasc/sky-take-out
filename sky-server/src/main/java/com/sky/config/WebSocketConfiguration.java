647ef2242190e029f254c946751723bfa7b8e3f1
Wang Xilai <1257811052@qq.com>
2025-02-06 18:37:44 +0800

    来单提醒

--- sky-server/src/main/java/com/sky/config/WebSocketConfiguration.java
+++ sky-server/src/main/java/com/sky/config/WebSocketConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 /**
  * WebSocket配置类，用于注册WebSocket的Bean 用于注册websocket 服务端组件
  * 通过这段代码配置，Spring会扫描项目中使用 ServerEndpoint注解声明的WebSocket端点，并自动注册这些端点，使它们能够被正确地映射和使用
+ * 用于实现来单提醒
  */
 @Configuration
 public class WebSocketConfiguration {
